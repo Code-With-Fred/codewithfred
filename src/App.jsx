161339842de6c2e6a40863a3f34bb5cd85582cb5
Code-With-Fred <ezefavourchimereze@gmail.com>
2025-05-09 21:38:31 +0100

    added about and service section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Portfolio from './Component/Portfolio'
+import About from './Component/about'
+import Services from './Component/services'
 
 export const App = () => {
   return (
  <div>
   <Portfolio/>
+  <About/>
+  <Services/>
  </div>
   )
 }
